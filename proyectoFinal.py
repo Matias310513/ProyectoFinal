97b5bf961cc400dc609fc2c91252242107261af3
Matias <matiasgomezmaya@gmail.com>
2026-03-28 14:48:27 -0500

    Creacion de los labels

--- proyectoFinal.py
+++ proyectoFinal.py
@@ -26,8 +26,50 @@ raiz = tk.Tk()
 raiz.title('Temperatura Ciudad')
 raiz.config(bg='gray20', bd=20, relief='solid')
 
+#----------------------------------------------------------------VARIABLES----------------------------------------------------------------
+fecha_actual = datetime.datetime.today().strftime('%A, %d-%B-%Y')
+hora_actual = datetime.datetime.now().time().strftime('%H:%M:%S')
+miNombre = tk.StringVar()
+miCorreo = tk.StringVar()
+miContrasena = tk.StringVar()
+GENERO = tk.IntVar()
+
 #----------------------------------------------------------------FRAME----------------------------------------------------------------
 mi_frame = tk.Frame()
 mi_frame.pack()
 mi_frame.config(width=1000, height=1000, bg='gray11', bd=11, relief='sunken')
 imagen_inicial = tk.PhotoImage(file='temperatura-img.png').subsample(x=2, y=2)
+
+#----------------------------------------------------------------LABELS----------------------------------------------------------------
+mi_titulo = tk.Label(mi_frame, text=f'Temperatura hoy: {fecha_actual}', fg='black', font=('Comic Sans Ms', 20))
+mi_titulo.grid(row=0, column=1, columnspan=2, pady=10)
+
+mi_hora = tk.Label(mi_frame, text=f'Hora: {hora_actual}', fg='black', font=('Comic Sans Ms', 20))
+mi_hora.grid(row=1, column=1, columnspan=2, pady=10)
+
+mi_imagen_label = tk.Label(mi_frame, image=imagen_inicial)
+mi_imagen_label.grid(row=2, column=0, columnspan=3, pady=10)
+
+mi_nombre = tk.Label(mi_frame, text='Nombre: ', fg='white', bg='gray11', font=('Comic Sans Ms', 10))
+mi_nombre.grid(row=3, column=0, padx=10, pady=10)
+
+mi_apellido = tk.Label(mi_frame, text='Apellido: ', fg='white', bg='gray11', font=('Comic Sans Ms', 10))
+mi_apellido.grid(row=4, column=0, padx=10, pady=10)
+
+mi_correo = tk.Label(mi_frame, text='Correo: ', fg='white', bg='gray11', font=('Comic Sans Ms', 10))
+mi_correo.grid(row=5, column=0, padx=10, pady=10)
+
+mi_contrasena = tk.Label(mi_frame, text='Contrasena: ', fg='white', bg='gray11', font=('Comic Sans Ms', 10))
+mi_contrasena.grid(row=6, column=0, padx=10, pady=10)
+
+mi_ciudad = tk.Label(mi_frame, text='Ingrese una ciudad: ', fg='white', bg='gray11', font=('Comic Sans Ms', 10))
+mi_ciudad.grid(row=7, column=0, padx=10, pady=10)
+
+mi_genero = tk.Label(mi_frame, text='Seleccione su genero: ', fg='white', bg='gray11', font=('Comic Sans Ms', 10))
+mi_genero.grid(row=3, column=1, padx=10, pady=10)
+
+mi_nombre_entry = tk.Entry(mi_frame, width=10, fg='gray', bg='gray15', justify='center', font=('Comic Sans Ms', 20), textvariable=miNombre)
+mi_nombre_entry.grid(row=2, column=1, padx=10, pady=10)
+
+mi_correo_entry = tk.Entry(mi_frame, width=10, fg='gray', bg='gray15', justify='center', font=('Comic Sans Ms', 20), textvariable=miCorreo)
+mi_correo_entry.grid(row=3, column=1, padx=10, pady=10)
